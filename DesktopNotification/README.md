dd7f1d96631f7c08bbd5a0e5797a8a749a2972ee
fandrade <felipe.andrade@jetty.com.br>
2017-06-07 15:14:09 -0300

    Simple Script to show popup news from a RSS Feed.

---
+++ DesktopNotification/README.md
@@ -0,0 +1,14 @@
+# Desktop Notification
+Simple script that reads a RSS feed and popup notifications on Desktop.
+
+You will need to use python3, pip to install some requirements.
+
+```
+# Ubuntu
+sudo apt install python3-pip
+pip3 install -r requirements.txt
+```
+
+To run the script, you need to call python3.
+
+`python3 news.py`
---
+++ DesktopNotification/news.py
@@ -0,0 +1,30 @@
+import feedparser
+import notify2
+import time
+import os
+
+def ParseFeed():
+    # You can choose what is the best RSS feed for you to follow
+    f = feedparser.parse('http://rss.home.uol.com.br/index.xml')
+
+    ICON_PATH = os.getcwd() + '/icon.ico'
+    notify2.init("News Notify")
+    # For each post in feed
+    for newsitem in f['items']:
+        msg = newsitem['summary']+"\n"+newsitem['link']
+        n = notify2.Notification(newsitem['title'],
+                                 msg,
+                                 icon=ICON_PATH)
+        # Define the Item to show in Desltop Notification
+        notify2.Notification(newsitem)
+        # Set the urgency
+        n.set_urgency(notify2.URGENCY_NORMAL)
+        # Show the Notification
+        n.show()
+        # Set 15 seconds to stay alive the popup notification
+        n.set_timeout(15000)
+        # Go to sleep
+        time.sleep(1200)
+
+if __name__ == '__main__':
+    ParseFeed()
